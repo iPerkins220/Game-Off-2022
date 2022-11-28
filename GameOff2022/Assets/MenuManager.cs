5569505ee9af83de66f49d88ef7ad47b67951949
iPerkins220 <62069122+iPerkins220@users.noreply.github.com>
2022-11-28 17:58:40 -0600

    worked on main menu

---
+++ GameOff2022/Assets/MenuManager.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public GameObject[] menuArray;
+
+    public GameObject[] rArray;
+
+    public int raInt = 0, lastInt;
+
+    private void Start()
+    {
+        menuArray[0].SetActive(true);
+    }
+
+    public void ToInfo()
+    {
+        menuArray[1].SetActive(true);
+        menuArray[0].SetActive(false);
+        menuArray[2].SetActive(false);
+    }
+
+    public void LevelSelect()
+    {
+        menuArray[2].SetActive(true);
+        menuArray[1].SetActive(false);
+    }
+
+    public void GoToRobots()
+    {
+        menuArray[3].SetActive(true);
+        menuArray[0].SetActive(false);
+    }
+
+    public void RightArrowClick()
+    {
+        raInt++;
+
+        if ((raInt) >= 5)
+        {
+            raInt = 0;
+            rArray[raInt].SetActive(true);
+            rArray[4].SetActive(false);
+            
+        }
+        lastInt = raInt-1;
+        rArray[raInt].SetActive(true);
+        rArray[lastInt].SetActive(false);
+        
+    }
+
+    public void LeftArrowClick()
+    {
+        raInt--;
+
+        if ((raInt) < 0)
+        {
+            raInt = 4;
+            rArray[raInt].SetActive(true);
+            rArray[0].SetActive(false);
+        }
+        lastInt = raInt + 1;
+        rArray[raInt].SetActive(true);
+        rArray[lastInt].SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        menuArray[0].SetActive(true);
+        menuArray[1].SetActive(false);
+        menuArray[3].SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
